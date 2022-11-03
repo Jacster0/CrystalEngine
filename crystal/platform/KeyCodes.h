1688c47401fc8f371c036b72d3b1e7f84890ee36
jaogren <jacob.ogren2@gmail.com>
2022-11-03 20:26:27 +0100

    prepare for windows support

--- crystal/platform/KeyCodes.h
+++ crystal/platform/KeyCodes.h
@@ -2,4 +2,6 @@
 
 #ifdef CRYSTAL_PLATFORM_LINUX
 #include "linux/Keycodes.h"
+#elifdef CRYSTAL_PLATFORM_WINDOWS
+#include "windows/Keycodes.h"
 #endif
--- crystal/platform/platform.h
+++ crystal/platform/platform.h
@@ -2,4 +2,6 @@
 
 #ifdef CRYSTAL_PLATFORM_LINUX
 #include "linux/platform.h"
+#elifdef CRYSTAL_PLATFORM_WINDOWS
+#include "windows/platform.h"
 #endif
