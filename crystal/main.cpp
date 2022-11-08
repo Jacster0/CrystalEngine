c38f7084b4502650a19b605e06f8edfdbf9f1d36
jaogren <jacob.ogren2@gmail.com>
2022-11-08 20:33:34 +0100

    Initial Vulkan commit

--- crystal/main.cpp
+++ crystal/main.cpp
@@ -3,24 +3,46 @@
 #include "core/logging/ConsoleSink.h"
 #include "core/input/Keyboard.h"
 #include <thread>
+#include <vulkan/vulkan.h>
 
 using namespace crystal;
 
-[[noreturn]] void process() {
-    while(true) {
-        if(Keyboard::key_is_pressed(KeyCode::J)) {
-            Logger::Info("Thread1 report: Key \"J\" was pressed");
-        }
+class Vulkan {
+public:
+    void init() {
+        createInstance();
     }
-}
+    void cleanUp() {
+        vkDestroyInstance(instance, nullptr);
+    }
+private:
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan Test";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "Crystal";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
-[[noreturn]] void process2() {
-    while(true) {
-        if(Keyboard::key_is_pressed(KeyCode::J)) {
-            Logger::Info("Thread2 report: Key \"J\" was pressed");
+        const char* extensionName = "VK_KHR_xcb_surface";
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = 1;
+        createInfo.ppEnabledExtensionNames = &extensionName;
+        createInfo.enabledLayerCount = 0;
+
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            Logger::Error("failed to create instance!");
+            throw std::runtime_error("failed to create instance!");
+        }
+        else {
+            Logger::Info("Success Creating VK instance!");
         }
     }
-}
+    VkInstance instance;
+};
 
 int main() {
     platform platform;
@@ -29,8 +51,8 @@ int main() {
 
     platform.create_main_window({100,100, 750, 512});
 
-    std::jthread t1(&process);
-    std::jthread t2(&process2);
+    Vulkan vulkan{};
+    vulkan.init();
 
     while(true) {
         if(const auto code = platform.process_messages()) {
@@ -39,5 +61,6 @@ int main() {
         }
     }
 
+    vulkan.cleanUp();
     crystal::Logger::remove_sink<ConsoleSink>();
 }
